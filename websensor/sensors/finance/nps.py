b273fd3933a85ac6bf9ed2243f7320495c6d8104
Gururaj Krishnamurthy <gururajk@qubole.com>
2020-07-17 23:52:46 +0530

    Adding nps

--- websensor/sensors/finance/nps.py
+++ websensor/sensors/finance/nps.py
@@ -5,7 +5,6 @@ import hashlib
 import os
 import pprint
 import re
-from time import sleep
 
 from PIL import Image, ImageEnhance, ImageFilter
 from bs4 import BeautifulSoup
@@ -72,15 +71,11 @@ class NpsSensor(BaseSensor):
         return result
 
     def get_id(self):
-        if "Please enter correct captcha code" in self.response.text:
-            haha
+        PP(self.response.text)
         lines = self.response.text.split('\n')
         PP([x for x in lines if 'var totalurl=' in x])
-        print(len(lines))
-        urllines = [x for x in lines if 'var totalurl=' in x]
-        print(urllines)
-        urlline = urllines[0]
-        match = re.match(r'.*\?ID=([\d-]+).*', urlline)
+        urlline = [x for x in lines if 'var totalurl=' in x][0]
+        match = re.match(r'.*\?ID=([^\&]+)&.*', r'\1', urlline)
         if not match:
             raise Exception("Cannot get ID")
         return match.groups()[0]
@@ -112,19 +107,6 @@ curl 'https://cra-nsdl.com/CRA/Log-Off.do?ID=-1282619975&getName=Log-Off' \
   --compressed
 '''
 
-def drop_to_shell(vars):
-    import code
-    try:
-        import readline
-        import rlcompleter
-        historyPath = os.path.expanduser("~/.pyhistory")
-        if os.path.exists(historyPath):
-                readline.read_history_file(historyPath)
-        readline.parse_and_bind('tab: complete')
-    except:
-        pass
-    code.interact(vars)
-
 def main(args) -> dict:
     """ Execute the command.
 
@@ -140,7 +122,7 @@ def main(args) -> dict:
     sensor = NpsSensor('finance/nps', URLS['base'], headers=headers,
                        creds=True)
 
-    @retry(CaptchaError, tries=1)
+    @retry(CaptchaError, tries=3)
     def solve_captcha():
         captcha = sensor.solve_captcha(URLS['login'])
         if captcha > 100:
@@ -184,6 +166,8 @@ def main(args) -> dict:
     # Lets get the required data
     logger.info("Getting the passbook page ..")
     sensor.get(URLS['passbook'])
+    if not sensor.soup.find('input', {'id': 'logout'}):
+        raise LoginError(f"Login did not work for username {data['username']}")
 
     h3 = sensor.soup.find('h3').text
     match = re.match(r'\s*Welcome\s*:\s*(.*)\s*\[\s*(\d+)\s*\]', h3)
